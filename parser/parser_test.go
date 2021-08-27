42f03031722ddb63f44c01da60206a3bb6e80a30
Thomas Leyh <thomas.leyh@mailbox.org>
2021-08-27 11:49:53 +0200

    bool parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -156,6 +156,50 @@ func TestIntegerLiteralExpression(t *testing.T) {
 	}
 }
 
+func TestBooleanExpression(t *testing.T) {
+	input := "true;false;"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 2 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+	boolean, ok := stmt.Expression.(*ast.Boolean)
+	if !ok {
+		t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
+	}
+	if boolean.Value != true {
+		t.Errorf("boolean.Value not %s. got=%t", "true", boolean.Value)
+	}
+	if boolean.TokenLiteral() != "true" {
+		t.Errorf("boolean.TokenLiteral not %s. got=%s", "true",
+			boolean.TokenLiteral())
+	}
+	stmt, ok = program.Statements[1].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[1] is not ast.ExpressionStatement. got=%T",
+			program.Statements[1])
+	}
+	boolean, ok = stmt.Expression.(*ast.Boolean)
+	if !ok {
+		t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
+	}
+	if boolean.Value != false {
+		t.Errorf("boolean.Value not %s. got=%t", "false", boolean.Value)
+	}
+	if boolean.TokenLiteral() != "false" {
+		t.Errorf("boolean.TokenLiteral not %s. got=%s", "false",
+			boolean.TokenLiteral())
+	}
+}
+
 func TestParsingPrefixExpression(t *testing.T) {
 	prefixTests := []struct {
 		input string
